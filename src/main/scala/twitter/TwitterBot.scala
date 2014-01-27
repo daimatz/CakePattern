7ffd44b72223e701db7c38a8e388841cf06c6093
daimatz <dai@daimatz.net>
2014-01-28 02:58:21 +0900

    add TwitterBot example

---
+++ src/main/scala/twitter/TwitterBot.scala
@@ -0,0 +1,21 @@
+package twitter
+
+case class Status(id: Int, userId: String, text: String)
+
+trait TwitterService {
+  def tweet(text: String, inReplyTo: Option[Int]): Int
+  def getTimeline(count: Int): Seq[Status]
+}
+
+class TwitterBot(twitterService: TwitterService) {
+  def eventLoop() {
+    val statusIds = action(10)
+    Thread.sleep(60 * 1000)
+  }
+  def action(count: Int): Seq[Int] = {
+    val tl = twitterService.getTimeline(count)
+    tl.map { status: Status =>
+      twitterService.tweet("@" + status.userId + " おやすみ〜", Some(status.id))
+    }
+  }
+}
---
+++ src/test/scala/twitter/TwitterBotSpec.scala
@@ -0,0 +1,22 @@
+package twitter
+
+import org.mockito._
+import org.specs2.mutable._
+
+class TwitterBotSpec extends Specification {
+  val twitterService = Mockito.mock(classOf[TwitterService])
+  val twitterBot = new TwitterBot(twitterService)
+  "action" should {
+    "return status ids" in {
+      val status1 = Status(id = 1, userId = "foo", text = "Hello")
+      val status2 = Status(id = 2, userId = "bar", text = "Scala")
+      val statuses = Seq(status1, status2)
+      Mockito.when(twitterService.getTimeline(2)).thenReturn(statuses)
+      val id1 = 100
+      val id2 = 102
+      Mockito.when(twitterService.tweet("@foo おやすみ〜", Some(1))).thenReturn(id1)
+      Mockito.when(twitterService.tweet("@bar おやすみ〜", Some(2))).thenReturn(id2)
+      twitterBot.action(2) must_== Seq(id1, id2)
+    }
+  }
+}
